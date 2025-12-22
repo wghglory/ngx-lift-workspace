62d8ac527299773a2974f3301485b68978ac2e02
Derek Wang <derek.wang@broadcom.com>
2025-12-22 21:31:04 +0800

    feat: create clr-lift lib

---
+++ ngx-lift-workspace/libs/clr-lift/README.md
@@ -0,0 +1,7 @@
+# clr-lift
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test clr-lift` to execute the unit tests.
---
+++ ngx-lift-workspace/libs/clr-lift/src/lib/clr-lift/clr-lift.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-clr-lift',
+  imports: [],
+  templateUrl: './clr-lift.html',
+  styleUrl: './clr-lift.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ClrLift {}
